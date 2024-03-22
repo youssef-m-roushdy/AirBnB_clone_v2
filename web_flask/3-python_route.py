027b85f4749e9b5778478b6d92bdfabe74f8fd69
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-03-22 14:54:08 +0200

    .

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace("_", " "))
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    return 'Python {}'.format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
